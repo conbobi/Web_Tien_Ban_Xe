5c5b5c05db6b9296a68ce3d34011f0732597fc24
conbobi <tienzed2003@gmail.com>
2025-03-03 12:47:48 +0700

    day code lab2 Nguyen Van Tien

---
+++ ASC.DataAccess/Reponsitory.cs
@@ -0,0 +1,63 @@
+﻿using ASC.DataAccess.Interfaces;
+using ASC.Model.Basetypes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.DataAccess
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity, new()
+    {
+        private readonly DbContext _dbContext;
+
+        public Repository(DbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            var entityToInsert = entity as BaseEntity;
+            entityToInsert.CreatedDate = DateTime.UtcNow;
+            entityToInsert.UpdatedDate = DateTime.UtcNow;
+            var result = await _dbContext.Set<T>().AddAsync(entity);
+            return result.Entity;
+        }
+
+        public void Update(T entity)
+        {
+            var entityToUpdate = entity as BaseEntity;
+            entityToUpdate.UpdatedDate = DateTime.UtcNow;
+            _dbContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            var entityToDelete = entity as BaseEntity;
+            entityToDelete.UpdatedDate = DateTime.UtcNow;
+            entityToDelete.IsDeleted = true;
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public async Task<T> FindAsync(string partitionKey, string rowKey)
+        {
+            var result = await _dbContext.Set<T>().FindAsync(partitionKey, rowKey);
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> FindAllByPartitionKeyAsync(string partitionKey)
+        {
+            var result =await _dbContext.Set<T>().Where(t => t.PartitionKey == partitionKey).ToListAsync();
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync()
+        {
+            var result = await _dbContext.Set<T>().ToListAsync();
+            return result;
+        }
+    }
+}
---
+++ ASC.Model/Basetypes/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.Model.Basetypes
+{
+    public class BaseEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+
+        public BaseEntity()
+        {
+            // Constructor có thể được sử dụng để khởi tạo các thuộc tính nếu cần thiết
+        }
+    }
+}
